1a9ae990200b1df72991f415dc4d0d934ebc9f09
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2025-01-13 11:42:41 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,9 @@
 provider "azurerm" {
   features {}
+  subscription_id = "b2cab71b-eb34-4062-9a43-dc2d606ec98f"  # Add your Azure Subscription ID here
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "example-resource-group"
   location = "Australia East"
 }
-
-
