49032ffde5b91aa3d72dd3da6df2b644292d1b64
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2025-01-13 12:01:33 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,6 +5,6 @@ subscription_id = "b2cab71b-eb34-4062-9a43-dc2d606ec98f"
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = var.example-resource-group
+  name     = var.resource_group_name
   location = var.location
 }
