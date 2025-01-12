f9915a3a54efa81439043c4043fd91c72532adcd
sandeepr1537 <164096457+sandeepr1537@users.noreply.github.com>
2025-01-13 12:00:49 +1300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,10 @@
 provider "azurerm" {
   features {}
+subscription_id = "b2cab71b-eb34-4062-9a43-dc2d606ec98f"
  
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resource-group"
-  location = "Australia East"
+  name     = var.example-resource-group
+  location = var.location
 }
