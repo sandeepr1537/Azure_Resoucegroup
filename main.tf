810c581077a9e88b396a56fc7ca98a0eb826ea13
sandeepr1537@gmail.com <sandeepr1537@gmail.com>
2025-01-13 14:35:50 +1300

    made changes to two files

--- main.tf
+++ main.tf
@@ -7,4 +7,10 @@ subscription_id = "b2cab71b-eb34-4062-9a43-dc2d606ec98f"
 resource "azurerm_resource_group" "example" {
   name     = var.resource_group_name
   location = var.location
+
+  tags = {
+    Environment   = "Production"
+    ServiceOwner  = "John Doe"
+    Department    = "IT"
+  }
 }
